975321566ab713c2d23bb2694cadd2b3ddcf600a
escalopa <ahmad.helaly.dev@gmail.com>
2024-11-17 18:25:19 +0300

    feat: solve varjoin

--- varjoin/join.go
+++ varjoin/join.go
@@ -2,6 +2,28 @@
 
 package varjoin
 
+import "strings"
+
 func Join(sep string, args ...string) string {
-	return ""
+	if len(args) == 0 {
+		return ""
+	}
+
+	bytes := len(sep) * (len(args) - 1)
+
+	for _, arg := range args {
+		bytes += len(arg)
+	}
+
+	var b strings.Builder
+	b.Grow(bytes)
+
+	for i, arg := range args {
+		b.WriteString(arg)
+		if i < len(args)-1 {
+			b.WriteString(sep)
+		}
+	}
+
+	return b.String()
 }
